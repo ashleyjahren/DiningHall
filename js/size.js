eb092709e98c3c6d592ccf8ee9ad78c757f62efe
ashleyjahren <ashleyjahren2012@u.northwestern.edu>
2016-04-11 01:00:18 -0500

    Uploading files

---
+++ js/size.js
@@ -0,0 +1,47 @@
+// Javascript Document
+var windowSize = '';
+var windowWidth = 0;
+var actualSize = 0; 
+
+$(document).ready(function(){
+
+	checkBrowserSize();
+	setInterval('checkBrowserSize()', 1000); 
+
+});
+
+function checkBrowserSize() {
+
+	windowWidth =  window.outerWidth; 
+	var contentWidth = $('body').width();
+	var sizeDiff = windowWidth - contentWidth; 
+	actualSize = windowWidth - sizeDiff; 
+
+
+	if(actualSize > 500 ) {newWindowSize = 'large';}
+	if (actualSize <= 500) {newWindowSize = 'small';}
+
+// $('h1').html(windowWidth+' ('+contentWidth+'+'+sizeDiff+')'+' is '+newWindowSize);
+
+if(windowSize != newWindowSize){
+	windowSize = newWindowSize; 
+	loadHero();
+	} 
+	// else{
+	// 	$('h1').append(' -- no change'); 
+	// }
+
+
+}
+
+function loadHero(){
+
+	if(windowSize == 'large'){
+		$('body').load("../index.html"); 
+
+	}
+	if(windowSize == 'small'){
+		$('body').load('../indexsmall.html'); 
+	}
+
+}
